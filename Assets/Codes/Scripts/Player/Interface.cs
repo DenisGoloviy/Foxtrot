8278b430311752aa2bc527f6b70fe0bf1c4b7e2a
DenisGoloviy <denis.goloviy@gmail.com>
2023-12-26 19:18:42 +0200

    Hpbar

--- Assets/Codes/Scripts/Player/Interface.cs
+++ Assets/Codes/Scripts/Player/Interface.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,9 +14,86 @@ public class Interface : MonoBehaviour
     public Image _Q;
     public Image _LM;
 
+    [Header("Bars:")]
+    public int _hp = 11;
+    public int _stamina = 10;
+    public Image _HPBAR;
+    public Image _STAMINABAR;
+
     void Update()
     {
         _Help();
+        _Bars();
+    }
+
+    void _Bars()
+    {
+        if(_hp > 11)
+        {
+            _hp = 11;
+        }
+        else if(_hp < 0) 
+        {
+            _hp = 0;
+        }
+
+        if(_stamina > 10) 
+        {
+            _stamina = 10;
+        }
+        else if(_stamina < 0)
+        {
+            _stamina = 0;
+        }
+
+        if(_hp == 11) 
+        {
+            _HPBAR.sprite = _ScriptObjectInterface._hpbar[0];
+        }
+        else if(_hp == 10) 
+        {
+            _HPBAR.sprite = _ScriptObjectInterface._hpbar[1];
+        }
+        else if (_hp == 9)
+        {
+            _HPBAR.sprite = _ScriptObjectInterface._hpbar[2];
+        }
+        else if (_hp == 8)
+        {
+            _HPBAR.sprite = _ScriptObjectInterface._hpbar[3];
+        }
+        else if (_hp == 7)
+        {
+            _HPBAR.sprite = _ScriptObjectInterface._hpbar[4];
+        }
+        else if (_hp == 6)
+        {
+            _HPBAR.sprite = _ScriptObjectInterface._hpbar[5];
+        }
+        else if (_hp == 5)
+        {
+            _HPBAR.sprite = _ScriptObjectInterface._hpbar[6];
+        }
+        else if (_hp == 4)
+        {
+            _HPBAR.sprite = _ScriptObjectInterface._hpbar[7];
+        }
+        else if (_hp == 3)
+        {
+            _HPBAR.sprite = _ScriptObjectInterface._hpbar[8];
+        }
+        else if (_hp == 2)
+        {
+            _HPBAR.sprite = _ScriptObjectInterface._hpbar[9];
+        }
+        else if (_hp == 1)
+        {
+            _HPBAR.sprite = _ScriptObjectInterface._hpbar[10];
+        }
+        else if (_hp == 0)
+        {
+            _HPBAR.sprite = _ScriptObjectInterface._hpbar[11];
+        }
     }
     void _Help()
     {
