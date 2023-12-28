f28026e09d85bd6862ef1985841b115cb8f0fb8c
DenisGoloviy <denis.goloviy@gmail.com>
2023-12-28 16:33:17 +0200

    HpBar

--- Assets/Codes/Scripts/Player/Interface.cs
+++ Assets/Codes/Scripts/Player/Interface.cs
@@ -16,7 +16,7 @@ public class Interface : MonoBehaviour
 
     [Header("Bars:")]
     public int _hp = 11;
-    public int _stamina = 10;
+    public int _stamina = 9;
     public Image _HPBAR;
     public Image _STAMINABAR;
 
@@ -37,62 +37,115 @@ public class Interface : MonoBehaviour
             _hp = 0;
         }
 
-        if(_stamina > 10) 
+        if(_stamina > 9) 
         {
-            _stamina = 10;
+            _stamina = 9;
         }
         else if(_stamina < 0)
         {
             _stamina = 0;
         }
 
-        if(_hp == 11) 
+        ///////HP:
+        if (_hp == 11) 
         {
             _HPBAR.sprite = _ScriptObjectInterface._hpbar[0];
         }
         else if(_hp == 10) 
         {
-            _HPBAR.sprite = _ScriptObjectInterface._hpbar[1];
+            _HPBAR.GetComponent<Animator>().enabled = true;
+            _HPBAR.GetComponent<Animator>().Play("10");
         }
         else if (_hp == 9)
         {
-            _HPBAR.sprite = _ScriptObjectInterface._hpbar[2];
+            _HPBAR.GetComponent<Animator>().enabled = true;
+            _HPBAR.GetComponent<Animator>().Play("9");
         }
         else if (_hp == 8)
         {
-            _HPBAR.sprite = _ScriptObjectInterface._hpbar[3];
+            _HPBAR.GetComponent<Animator>().enabled = true;
+            _HPBAR.GetComponent<Animator>().Play("8");
         }
         else if (_hp == 7)
         {
-            _HPBAR.sprite = _ScriptObjectInterface._hpbar[4];
+            _HPBAR.GetComponent<Animator>().enabled = true;
+            _HPBAR.GetComponent<Animator>().Play("7");
         }
         else if (_hp == 6)
         {
-            _HPBAR.sprite = _ScriptObjectInterface._hpbar[5];
+            _HPBAR.GetComponent<Animator>().enabled = true;
+            _HPBAR.GetComponent<Animator>().Play("6");
         }
         else if (_hp == 5)
         {
-            _HPBAR.sprite = _ScriptObjectInterface._hpbar[6];
+            _HPBAR.GetComponent<Animator>().enabled = true;
+            _HPBAR.GetComponent<Animator>().Play("5");
         }
         else if (_hp == 4)
         {
-            _HPBAR.sprite = _ScriptObjectInterface._hpbar[7];
+            _HPBAR.GetComponent<Animator>().enabled = true;
+            _HPBAR.GetComponent<Animator>().Play("4");
         }
         else if (_hp == 3)
         {
-            _HPBAR.sprite = _ScriptObjectInterface._hpbar[8];
+            _HPBAR.GetComponent<Animator>().enabled = true;
+            _HPBAR.GetComponent<Animator>().Play("3");
         }
         else if (_hp == 2)
         {
-            _HPBAR.sprite = _ScriptObjectInterface._hpbar[9];
+            _HPBAR.GetComponent<Animator>().enabled = true;
+            _HPBAR.GetComponent<Animator>().Play("2");
         }
         else if (_hp == 1)
         {
-            _HPBAR.sprite = _ScriptObjectInterface._hpbar[10];
+            _HPBAR.GetComponent<Animator>().enabled = true;
+            _HPBAR.GetComponent<Animator>().Play("1");
         }
         else if (_hp == 0)
         {
-            _HPBAR.sprite = _ScriptObjectInterface._hpbar[11];
+            _HPBAR.GetComponent<Animator>().enabled = true;
+            _HPBAR.GetComponent<Animator>().Play("0");
+        }
+        ///////Stamina:
+        if (_stamina == 9)
+        {
+            _STAMINABAR.sprite = _ScriptObjectInterface._staminabar[9];
+        }
+        else if (_stamina == 8)
+        {
+            _STAMINABAR.sprite = _ScriptObjectInterface._staminabar[8];
+        }
+        else if (_stamina == 7)
+        {
+            _STAMINABAR.sprite = _ScriptObjectInterface._staminabar[7];
+        }
+        else if (_stamina == 6)
+        {
+            _STAMINABAR.sprite = _ScriptObjectInterface._staminabar[6];
+        }
+        else if (_stamina == 5)
+        {
+            _STAMINABAR.sprite = _ScriptObjectInterface._staminabar[5];
+        }
+        else if (_stamina == 4)
+        {
+            _STAMINABAR.sprite = _ScriptObjectInterface._staminabar[4];
+        }
+        else if (_stamina == 3)
+        {
+            _STAMINABAR.sprite = _ScriptObjectInterface._staminabar[3];
+        }
+        else if (_stamina == 2)
+        {
+            _STAMINABAR.sprite = _ScriptObjectInterface._staminabar[2];
+        }
+        else if (_stamina == 1)
+        {
+            _STAMINABAR.sprite = _ScriptObjectInterface._staminabar[1];
+        }
+        else if (_stamina == 0)
+        {
+            _STAMINABAR.sprite = _ScriptObjectInterface._staminabar[0];
         }
     }
     void _Help()
