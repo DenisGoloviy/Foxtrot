2fd03a9ba329fef28476db49295c2f3bf253d9d4
DarkShadowAssasinSniper <nikitogerovski@gmail.com>
2024-01-05 20:22:10 +0200

    Update EnemyAI.cs

--- Assets/Codes/Scripts/EnemyAI.cs
+++ Assets/Codes/Scripts/EnemyAI.cs
@@ -8,21 +8,45 @@ public class EnemyAI : MonoBehaviour
 
     Rigidbody2D rb;
     BoxCollider2D boxCollider;
+    CircleCollider2D circleCollider;
+
+    public Transform playerPosition;
+    public bool isChasing;
+    public float chaseDistance;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
+        circleCollider = GetComponent<CircleCollider2D>();
     }
     void Update()
     {
-        if (IsFacingRight())
+        if (isChasing)
         {
-            rb.velocity = new Vector2(moveSpeed, 0f);
+            if (transform.position.x > playerPosition.position.x)
+            {
+                transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+            }
+            if (transform.position.x < playerPosition.position.x)
+            {
+                transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+            }
         }
         else
         {
-            rb.velocity = new Vector2(-moveSpeed, 0f);
+            if(Vector2.Distance(transform.position, playerPosition.position) < chaseDistance)
+            {
+                isChasing = false;
+            }
+            if (IsFacingRight())
+            {
+                rb.velocity = new Vector2(moveSpeed, 0f);
+            }
+            else
+            {
+                rb.velocity = new Vector2(-moveSpeed, 0f);
+            }
         }
     }
 
